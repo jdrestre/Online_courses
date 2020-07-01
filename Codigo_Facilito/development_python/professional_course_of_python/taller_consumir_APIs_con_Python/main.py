6749e4e314850658fe5b2a5b6862e5c400866465
jdrestre <jdrestre79@gmail.com>
2020-06-30 20:01:09 -0500

    try to download video

--- Codigo_Facilito/development_python/professional_course_of_python/taller_consumir_APIs_con_Python/main.py
+++ Codigo_Facilito/development_python/professional_course_of_python/taller_consumir_APIs_con_Python/main.py
@@ -3,8 +3,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = 'https://esc365.escardio.org/Congress/EuroCMR-2019/Closing-plenary-what-is-in-the-crystal-ball-of-CMR-from-Venice-to-Mars/186966-cmr-imaging-of-myocardial-fibers-do-we-need-it#slide'
-
+    url = 'https://esc365.escardio.org/static-resources/ESC365/videoplayer/cyimplayer/2.0.7/player.html?mediaurl=https%3A%2F%2Fd381ul1ljfghfa.cloudfront.net%2FEUROCMR2019%2FScientific%2520Programme%2F186966%2FVOD%2F&signature=%3FPolicy%3DeyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vZDM4MXVsMWxqZmdoZmEuY2xvdWRmcm9udC5uZXQvRVVST0NNUjIwMTkvU2NpZW50aWZpYyUyMFByb2dyYW1tZS8xODY5NjYvKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTU5MzU3NjIxOH0sIklwQWRkcmVzcyI6eyJBV1M6U291cmNlSXAiOiIwLjAuMC4wLzAifX19XX0_%26Signature%3DOswrdRoXWW2PT8fXCWYKa9%7EW1TpTrtTUL62OVMIpt1xyvl%7EovB88yPq-TL--BkzFTerXYto-naNA--rasA-CPJogeSBogDtBpdS0agU14JQ97n1EzVTI77h2q0qPL2SOVOZ7oOBhS5LDqQSd9WuSfQX1a4RnM9zXiYb7y9E0NwCa7tErRwB2R%7El8N-GOnMpigexT0-G6%7E-kX8MiESZTFXfhgwLeCmLKUKhfusCHTg2ehuXK%7EC4ENepejFfRwGoRoOpptPZkeXJ5GtVkttZG9KKPqrR-TeUsHqncXdytDtggWZcHkLx8zydqN3zLHoGkcS4EiVPHmW1aEVkt%7ELWritg__%26Key-Pair-Id%3DAPKAIWNNOMINYCK536DA&itemid=ESC365PRESENTATION186966&itemTitle=CMR+imaging+of+myocardial+fibers%3A+do+we+need+it%3F&itemEvent=EuroCMR+2019'
     response = requests.get(url, stream=True)
     with open('video.mp4', 'wb') as file:
         for chunk in response.iter_content():
